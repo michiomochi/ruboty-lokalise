818b514be9481728d1b454dfcbd542fbb13a39f3
Michikawa Masayoshi <michikawa.masayoshi@gmail.com>
2020-01-22 20:57:27 +0900

    Initial commit

---
+++ lib/ruboty/lokalise.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "ruboty/lokalise/version"
+
+module Ruboty
+  module Lokalise
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
